fef8a0f763b5765ca391bcb93827d9a862b04671
강상원 <kevink1113@u.sogang.ac.kr>
2024-03-24 22:13:57 +0900

    💄 Changed UI components, indentation

--- frontend/lib/home.dart
+++ frontend/lib/home.dart
@@ -1,101 +1,100 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'dart:async';
-import 'dart:math';
 
-
-class HomePage extends StatefulWidget{
-  const HomePage({Key? key}) : super(key:key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
   @override
   _HomePage createState() => _HomePage();
 }
 
-class _HomePage extends State<HomePage>{
+class _HomePage extends State<HomePage> {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-
   }
+
   @override
   Widget build(BuildContext context) {
     var screenwidth = MediaQuery.of(context).size.width;
     // TODO: implement build
 
     return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Center(
+        key: scaffoldKey,
+        backgroundColor: Colors.white,
+        //
+        appBar: AppBar(
+          title: const Center(
             child: Text(
-                'Sogang Link',
+              'Sogang Link',
               textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white, // Set text color to white
+              ),
             ),
+          ),
+          backgroundColor: const Color(0xff9e2a2f),
         ),
-        backgroundColor: Colors.purple,
-      ),
-      body: const SingleChildScrollView(
-        child: Column(
-          children: [
-            Text('asdfa'),
-          ],
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('asdfa'),
+            ],
+          ),
         ),
-      ),
-      bottomNavigationBar: const BottomAppBar(
-        height:50 ,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Flexible(
-              child: Column(
-                children: [
-                  Icon(Icons.dehaze),
-                  Text('기능'),
-                ],
+        bottomNavigationBar: const BottomAppBar(
+          height: 68,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Column(
+                  children: [
+                    Icon(Icons.dehaze),
+                    Text('기능'),
+                  ],
+                ),
               ),
-              flex: 1,
-            ),
-            Flexible(
-              child: Column(
-                children: [
-                  Icon(Icons.date_range),
-                  Text('시간표'),
-                ],
+              Flexible(
+                flex: 1,
+                child: Column(
+                  children: [
+                    Icon(Icons.date_range),
+                    Text('시간표'),
+                  ],
+                ),
               ),
-              flex: 1,
-            ),
-            Flexible(
-              child: Column(
-                children: [
-                  Icon(Icons.home),
-                  Text('홈'),
-                ],
+              Flexible(
+                flex: 1,
+                child: Column(
+                  children: [
+                    Icon(Icons.home),
+                    Text('홈'),
+                  ],
+                ),
               ),
-              flex: 1,
-            ),
-            Flexible(
-              child: Column(
-                children: [
-                  Icon(Icons.mode_comment),
-                  Text('게시판'),
-                ],
+              Flexible(
+                flex: 1,
+                child: Column(
+                  children: [
+                    Icon(Icons.mode_comment),
+                    Text('게시판'),
+                  ],
+                ),
               ),
-              flex: 1,
-            ),
-            Flexible(
-              child: Column(
-                children: [
-                  Icon(Icons.settings),
-                  Text('설정'),
-                ],
+              Flexible(
+                flex: 1,
+                child: Column(
+                  children: [
+                    Icon(Icons.settings),
+                    Text('설정'),
+                  ],
+                ),
               ),
-              flex: 1,
-            ),
-          ],
-        ),
-      )
-    );
+            ],
+          ),
+        ));
     throw UnimplementedError();
   }
 }
