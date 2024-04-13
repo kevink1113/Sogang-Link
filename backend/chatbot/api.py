a09b462f2397b83bbd2c7fdfd15fc9ffa563cb45
shyoon1127 <shyoon1127@gmail.com>
2024-04-14 01:53:03 +0900

    add api function calling on chatbot

---
+++ backend/chatbot/api.py
@@ -0,0 +1,14 @@
+from lecture.models import Course, Takes
+from users.models import User
+from users.serializers import *
+def get_user_info(username):
+    user = User.objects.filter(username=username)
+    return UserSerializer(user, many=True).data
+
+def get_course_info():
+    course = Course.objects.all()
+    return CourseSerializer(course, many=True).data
+
+def get_takes_info(username):
+    takes = Takes.objects.filter(student_id=username)
+    return TakesSerializer(takes, many=True).data
