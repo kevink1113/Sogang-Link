cab1fc70b2796e6c2d4373c258b3693f66c184f0
강상원 <kevink1113@u.sogang.ac.kr>
2024-04-27 12:54:50 +0900

    ⚡️ Chatbot function call modified

--- backend/chatbot/chatbot.py
+++ backend/chatbot/chatbot.py
@@ -76,28 +76,48 @@ def chatbot_function_call(runs, assistant_id, user, thread_id):
     )
 
 
+def cancel_active_runs(client, thread_id):
+    """
+    명시된 thread에서 실행중인 모든 run을 취소하는 함수
+
+    :param client: 사용할 OpenAI client
+    :param thread_id: 실행중인 active run(들)이 있는 thread ID
+    """
+
+    active_runs = client.beta.threads.runs.list(thread_id=thread_id).data
+    for run in active_runs:
+        if run.status not in ["completed", "failed", "cancelled"]: # 취소된 run은 다시 취소하지 않음
+            client.beta.threads.runs.cancel(thread_id=thread_id, run_id=run.id)
+            print(f"Cancelled run {run.id}")
+
+
 def chatbot_query(assistant_id, user, thread_id, question):
+    # 만약 이전에 진행중인 run이 있다면 취소
+    cancel_active_runs(client, thread_id)
+
+    # 클리어 한 다음 새로운 질문을 추가
     client.beta.threads.messages.create(
         thread_id=thread_id,
         role="user",
         content=question
     )
+
+    # 새로운 run 생성
     runs = client.beta.threads.runs.create(
-            thread_id=thread_id,
-            assistant_id=assistant_id
+        thread_id=thread_id,
+        assistant_id=assistant_id
     )
-    while runs.status != "completed":
-        if runs.status == "requires_action":
-            chatbot_function_call(runs, assistant_id, user, thread_id)
-        runs = client.beta.threads.runs.retrieve(
-            thread_id=thread_id,
-            run_id=runs.id
-        )
-        # time.sleep(0.5)
-    messages = client.beta.threads.messages.list(
-            thread_id=thread_id
-        )
+
+    # run이 완료되거나 실패할 때까지 기다림
+    while runs.status not in ["completed", "failed"]:
+        if runs.status == "requires_action": # 만약 function call이 필요하다면
+            chatbot_function_call(runs, assistant_id, user, thread_id) 
+        runs = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=runs.id)
+
+    # run이 완료되면 메시지 반환
+    messages = client.beta.threads.messages.list(thread_id=thread_id)
     return messages
+
 
 
 class EventHandler(AssistantEventHandler):
