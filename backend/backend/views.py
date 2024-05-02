f3dc814586553d7fd5b65ad57c13492f2da2ba07
강상원 <kevink1113@u.sogang.ac.kr>
2024-05-02 22:44:23 +0900

    ✅ StreamingHttpResponse

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -8,12 +8,24 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from .crawl_saint import get_saint_cookies
 from .auth_backend import PasswordlessAuthBackend
-from django.contrib.auth import login as auth_login
 
+import openai
+from chatbot.secret import get_secret
+from chatbot.api import *
+
+from django.contrib.auth import login as auth_login
+from typing_extensions import override
+from openai.types.beta.threads.runs import ToolCall, RunStep
+
+from openai.types.beta.assistant_stream_event import (
+    ThreadRunRequiresAction, ThreadMessageDelta, ThreadRunCompleted,ThreadMessageCompleted,
+    ThreadRunFailed, ThreadRunCancelling, ThreadRunCancelled, ThreadRunExpired, ThreadRunStepFailed,
+    ThreadRunStepCancelled)
 # from backend.chatbot import chatbot_function_call
 # from chatbot import chatbot_function_call
 from chatbot.chatbot import chatbot_function_call
 
+client = openai.OpenAI(api_key=get_secret())
 
 from drf_yasg.utils import swagger_auto_schema
 import openai
@@ -114,6 +126,20 @@ def my_login_view(request):
 def offline(request):
     return render(request, 'offline.html')
 
+def cancel_active_runs(client, thread_id):
+    """
+    명시된 thread에서 실행중인 모든 run을 취소하는 함수
+
+    :param client: 사용할 OpenAI client
+    :param thread_id: 실행중인 active run(들)이 있는 thread ID
+    """
+
+    active_runs = client.beta.threads.runs.list(thread_id=thread_id).data
+    for run in active_runs:
+        if run.status not in ["completed", "failed", "cancelled", "expired"]: # 취소된 run은 다시 취소하지 않음
+            client.beta.threads.runs.cancel(thread_id=thread_id, run_id=run.id)
+            print(f"Cancelled run {run.id}")
+
 
 class ChatView(APIView):
     """
@@ -133,42 +159,138 @@ class ChatView(APIView):
         }
         # response 정의
     ))
-    # @swagger_auto_schema(operation_description="POST 요청을 위한 엔드포인트입니다.")
+    # def post(self, request, *args, **kwargs):
+    #     user = request.user
+
+    #     print(user)
+    #     print("Question: ", request.data.get('question'))
+
+    #     question = request.data.get('question')
+    #     assistant_id = "asst_fSEoeHlDpbVT7NA4chr18jLM"
+    #     thread_id = user.thread
+
+    #     chatbot_query_stream(assistant_id, user, thread_id, question)
+
+    #     recent_question = messages.data[1].content[0].text.value
+    #     recent_answer = messages.data[0].content[0].text.value
+
+    #     print(recent_question)
+    #     print(recent_answer)
+    #     return Response({'answer': recent_answer},
+    #                     status=status.HTTP_200_OK)
+
     def post(self, request, *args, **kwargs):
         user = request.user
-
-        print(user)
-        print("Question: ", request.data.get('question'))
-
         question = request.data.get('question')
         assistant_id = "asst_fSEoeHlDpbVT7NA4chr18jLM"
-        #assist 나중에 숨기거나 해야하나 안숨겨도 별 문제는 없긴함
-        # thread_id = "thread_QkJXOaYm6rzUUcsF1xhZh9DU"
         thread_id = user.thread
-
-        # ========== 그냥 한번에 버전, stream 버전 ==========
-
-        messages = chatbot_query(assistant_id, user, thread_id, question) # TODO: chatbot_query_stream으로 변경
-        # messages = chatbot_query_stream(assistant_id, user, thread_id, question)
-        # chatbot_query_setrem은 message 반환 안해서 변경함
-        chatbot_query_stream(assistant_id, user, thread_id, question)
-        # ===================== 디버깅용 출력 =====================
-        total_message = "대화:\n"
-        for i, message in enumerate(reversed(messages.data), start=1):
-            total_message += "서강gpt>" if message.role == "assistant" else "당신>"
-            for content in message.content:
-                total_message += content.text.value + "\n"
-        print("total message: ", total_message)
-        # ===================== 디버깅용 출력 =====================
-
-        recent_question = messages.data[1].content[0].text.value
-        recent_answer = messages.data[0].content[0].text.value
-
-        print(recent_question)
-        print(recent_answer)
-        return Response({'answer': recent_answer},
-                        status=status.HTTP_200_OK)
         
+        cancel_active_runs(client, thread_id)
+
+        def event_stream():
+            handler = EventHandler(thread_id=thread_id, assistant_id=assistant_id, user=user)
+            # Initialize the streaming process
+            # 질문 보내기
+            client.beta.threads.messages.create(
+                thread_id=thread_id,
+                role="user",
+                content=question
+            )
+            # 스트림으로 받기
+            with client.beta.threads.runs.stream(
+                    thread_id=thread_id,
+                    assistant_id=assistant_id,
+                    event_handler=handler,
+            ) as stream:
+                try:
+                    # Yield data as server-sent events
+                    for event in stream:
+                        if isinstance(event, ThreadMessageDelta):
+                            # 메시지 델타 이벤트 처리
+                            data = event.data.delta.content
+                            for text in data:
+                                print(text.text.value, end='')
+                                yield f"data: {json.dumps({'text': text.text.value})}\n\n"
+                                # yield "data: run_writing\n\n"
+                                # yield text.text.value 
+                            # yield f"data: {'text_update': event.data.delta.content}\n\n"
+                        # elif isinstance(event, ThreadMessageCompleted):
+                            # 메시지 완료 이벤트 처리
+
+                            # yield f"data: {'text': event.data.content}\n\n"
+                        elif isinstance(event, ThreadRunCompleted):
+                            # 실행 완료 이벤트 처리
+                            yield "data: run_completed\n\n"
+                        # else:
+                        #     # 처리하지 못한 이벤트 타입 로깅
+                        #     print(f"Unhandled event type: {type(event)}")
+                        #     yield f"data: {json.dumps({'error': 'Unhandled event type'})}\n\n"
+
+                except GeneratorExit:
+                    # Handle the case when the client disconnects
+                    stream.close()
+
+        # Return a StreamingHttpResponse that keeps the connection open
+        response = StreamingHttpResponse(event_stream(), content_type='text/event-stream')
+        response['Cache-Control'] = 'no-cache'
+        return response
+
+class EventHandler(AssistantEventHandler):
+    def __init__(self, thread_id, assistant_id, user):
+        super().__init__()
+        self.thread_id = thread_id
+        self.assistant_id = assistant_id
+        self.run_id = None
+        self.user = user
+        self.tool_outputs = []
+
+    @override
+    def on_text_created(self, text) -> None:
+        print(f"\n서강gpt > ", end="", flush=True)
+
+    @override
+    def on_text_delta(self, delta, snapshot):
+        print(delta.value, end="", flush=True)
+
+    @override
+    def on_tool_call_done(self, tool_call: ToolCall):
+        # run 참조
+        run = client.beta.threads.runs.retrieve(
+            thread_id=self.thread_id,
+            run_id=self.run_id)
+        # 처음 tool_call에서 모든 tool_call을 처리
+        if run.status == "requires_action":
+            tools = run.required_action.submit_tool_outputs.tool_calls
+            for tool in tools:
+                tool_id = tool.id
+                function_args = tool.function.arguments
+                function_name = tool.function.name
+                data = ""
+
+                #함수 하드 코딩 안 하는 방법이 있긴 한데, 좀 가독성이 구려서 그냥 하드코딩 합시다.
+                if function_name == "get_user_info":
+                    data = get_user_info(self.user.username)
+                elif function_name == "get_course_info":
+                    data = get_course_info()
+                elif function_name == "get_takes_info":
+                    data = get_takes_info(self.user.username)
+                self.tool_outputs.append({
+                    "tool_call_id": tool_id,
+                    "output": json.dumps(data)
+                })
+            #스트림으로 보내기
+            with client.beta.threads.runs.submit_tool_outputs_stream(
+                    thread_id=self.thread_id,
+                    run_id=self.run_id,
+                    tool_outputs=self.tool_outputs,
+                    event_handler=EventHandler(self.thread_id, self.assistant_id, self.user)
+            ) as stream:
+                stream.until_done()
+
+    @override
+    def on_run_step_created(self, run_step: RunStep):
+        # run_id 저장
+        self.run_id = run_step.run_id
 
 
 class StreamView(APIView):
