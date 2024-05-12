c93e2bae088828d47f8ba2b7a8d0f49f0ace1048
luna156 <v010309@naver.com>
2024-05-12 21:56:27 +0900

    파일추가

---
+++ frontend/lib/tools.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:soganglink/data/courses/takes.dart';
+import 'package:soganglink/data/login/User.dart';
+import 'package:soganglink/login.dart';
+import 'package:soganglink/storage.dart';
+import 'package:soganglink/tools/calcgrade.dart';
+import 'package:wakelock/wakelock.dart';
+import 'package:screen_brightness/screen_brightness.dart';
+
+class Tools extends StatefulWidget {
+  const Tools({Key? key}) : super(key: key);
+  @override
+  _Tools createState() => _Tools();
+}
+
+class _Tools extends State<Tools> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    void initState() {
+      // TODO: implement initState
+      super.initState();
+    }
+
+    // TODO: implement build
+    return Container(
+        alignment: Alignment(-0.95, -1.0),
+        decoration: BoxDecoration(
+          color: Colors.white, // Container의 배경색
+          borderRadius: BorderRadius.circular(20), // 둥근 모서리 반경 설정
+          // border: Border.all(
+          //   color: Colors.blue, // 테두리 색상
+          //   width: 2, // 테두리 두께
+          // ),
+        ),
+        margin: EdgeInsets.fromLTRB(20, 20, 20, 30),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              InkWell(
+                child: Center(
+                    child: Text(
+                  "성적계산기",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                  ),
+                )),
+                onTap: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => CalcGrade()));
+                },
+              )
+            ],
+          ),
+        ));
+  }
+}
