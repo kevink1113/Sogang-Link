4d0540f64fa6c6be185b441fe8acafae69c4d0fb
강상원 <kevink1113@u.sogang.ac.kr>
2024-07-22 22:26:29 +0900

    ✅ Push Notification (FCM)

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,69 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:soganglink/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String? _token;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeFirebaseMessaging();
+  }
+
+  void _initializeFirebaseMessaging() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    // Request permission for iOS
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('User granted provisional permission');
+    } else {
+      print('User declined or has not accepted permission');
+    }
+
+    // Get the token each time the application loads
+    String? token = await messaging.getToken();
+    print("Firebase Messaging Token: $token");
+    setState(() {
+      _token = token;
+    });
+
+    // Handle foreground messages
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+
+    // Handle background messages
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+      print('Message data: ${message.data}');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Sogang Link",
       theme: ThemeData(
-          useMaterial3: true,
-          fontFamily: GoogleFonts.notoSansKr().fontFamily,
-          // fontFamily: GoogleFonts.ibmPlexSans().fontFamily,
-          // Define the default brightness and colors.
-          scaffoldBackgroundColor: const Color(0xFFf5f5f5),
-          dialogBackgroundColor: const Color(0xFFf5f5f5),
-          cardColor: Colors.white,
-          // scaffoldBackgroundColor: Colors.grey.shade200,
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color(0xFF9e2a2f),
-            // brightness: Brightness.dark,
-            // seedColor: Colors.white,
-            // TRY THIS: Change to "Brightness.light"
-            //           and see that all colors change
-            //           to better contrast a light background.
-            // brightness: Brightness.dark,
+        useMaterial3: true,
+        fontFamily: GoogleFonts.notoSansKr().fontFamily,
+        scaffoldBackgroundColor: const Color(0xFFf5f5f5),
+        dialogBackgroundColor: const Color(0xFFf5f5f5),
+        cardColor: Colors.white,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF9e2a2f),
+        ),
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
           ),
-          textTheme: const TextTheme(
-            titleLarge: TextStyle(
-              // color: Colors.black,
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
-              // letterSpacing: 2.0,
-            ),
-            // bodyMedium: GoogleFonts.notoSansKr(),
-          )
-          // Define the default `TextTheme`. Use this to specify the default
-          // text styling for headlines, titles, bodies of text, and more.
-          // textTheme: TextTheme(
-          //   displayLarge: const TextStyle(
-          //     fontSize: 72,
-          //     fontWeight: FontWeight.bold,
-          //   ),
-          //   // TRY THIS: Change one of the GoogleFonts
-          //   //           to "lato", "poppins", or "lora".
-          //   //           The title uses "titleLarge"
-          //   //           and the middle text uses "bodyMedium".
-          //   titleLarge: GoogleFonts.oswald(
-          //     fontSize: 30,
-          //     fontStyle: FontStyle.italic,
-          //   ),
-          //   bodyMedium: GoogleFonts.notoSansKr(
-          //     fontSize: 16,
-          //     fontWeight: FontWeight.w500,
-          //   ),
-          //   displaySmall: GoogleFonts.pacifico(),
-          // ),
-          ),
-      home: const Login(),
+        ),
+      ),
+      home: const Login(), // 전달된 token을 Login 페이지로 전달
     );
   }
 }
--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import file_selector_macos
 import firebase_core
+import firebase_messaging
 import flutter_secure_storage_macos
 import path_provider_foundation
 import screen_brightness_macos
@@ -17,6 +18,7 @@ import wakelock_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
